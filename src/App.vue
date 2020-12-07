6dcc3bee1d0b28c026bc920d506587519a85f3c0
sochief <shafkatoo@gmail.com>
2020-12-07 13:33:11 +0300

    Creaating page navigation without loading another page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item active"></li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/cars">Cars</router-link>
+          </li>
+          <li class="nav-item"></li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>

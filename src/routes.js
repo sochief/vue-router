22793769b4c042c5320fef5a971c7eef4ffe6c07
sochief <shafkatoo@gmail.com>
2020-12-09 14:53:40 +0300

    Redirect to 404 for example

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Cars from "./pages/Cars";
 import Car from "./pages/Car";
 import CarFull from "./pages/CarFull";
+import ErrorCmp from "./pages/Error";
 
 export default new VueRouter({
   routes: [
@@ -12,7 +13,8 @@ export default new VueRouter({
     },
     {
       path: "/cars",
-      component: Cars
+      component: Cars,
+      name: "cars"
     },
     {
       path: "/car/:id",
@@ -24,6 +26,16 @@ export default new VueRouter({
           name: "carFull"
         }
       ]
+    },
+    {
+      path: "/none",
+      redirect: {
+        name: "cars"
+      }
+    },
+    {
+      path: "*",
+      redirect: ErrorCmp
     }
   ],
   mode: "history",
